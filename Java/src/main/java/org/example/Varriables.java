a670e79be7733285d6ce992cc0ae10c0d01cec95
ZulfiqarAli111 <zulfiqarkhokhar111@gmail.com>
2026-02-10 13:05:12 +0500

    variables primitive types

---
+++ Java/src/main/java/org/example/Varriables.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class Varriables {
+
+    public static void main(String[] args) {
+        // declair varribale
+        int age;
+
+        // initialize the variable
+        age = 10;
+
+        //Primitive data type
+        //numbers
+
+        //8 bit or 1 byte
+        byte minByteValue = -128;
+        byte maxByteValue = 127;
+
+        // 16 bit or 2 byte
+        short shortMinValue = -32768;
+        short shortMaxValue = 32767;
+
+        //32 bit or 4 byte
+        int minIntValue = Integer.MIN_VALUE;
+        int maxIntValue = Integer.MAX_VALUE;
+
+        //64 bit or 8 bytes
+        long minLongValue = Long.MIN_VALUE;
+        long maxLongValue = Long.MAX_VALUE;
+
+        //floats
+
+        //32 byte or 4 bit
+        float myFloat = 10.5f;
+
+        //64 byte or 8 byte
+        double myDouble = 10.5;
+
+        //characters
+
+        char myChar = 'A';
+        char myChar2 = '#';
+
+        //boolean
+
+        boolean myBoolean = true;
+        boolean myBoolean2 = false;
+    }
+}
